fe3a73f62a54ead4f59714ce7833e2997b797d7e
Koihcire <ericchiok@gmail.com>
2022-08-01 09:38:14 +0000

    test

---
+++ dal/cart_items.js
@@ -0,0 +1,11 @@
+const { CartItem } = require('../models');
+
+const getCart = async (userId) => {
+    return await CartItem.collection()
+        .where({
+            'user_id': userId
+        }).fetch({
+            require: false,
+            withRelated: ['product', 'product.category']
+        });
+}
